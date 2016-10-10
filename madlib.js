180ddff9496c0a562a823075303e0fc6ef3f3fda
Manpreet Singh <mannyy91s@gmail.com>
2016-10-10 13:22:19 -0400

    finished making vars and typing the story

---
+++ madlib.js
@@ -0,0 +1,25 @@
+var fillInBlankerName;
+var dayOfWeek;
+var sleepOn;
+var whatDoYouTakeInTheMorning;
+var doToHair;
+var favoriteBreakfast;
+var noun1;
+var doInClass;
+var occupation;
+var noun2;
+var noun3;
+var randomFemaleName;
+var adjective1;
+var verb1;
+var noun4;
+var bodyPart;
+var adjective2;
+var nounPlural;
+var noun5;
+var preposition;
+var doBeforeBed;
+
+var sentence = `Hey, my name is ${fillInBlankerName} and this is my college story. Every ${dayOfWeek}, I wake up and get off my ${sleepOn}. I don't always take a ${whatDoYouTakeInTheMorning}, but I always make sure to ${doToHair} my hair. Once that is done I get some ${favoriteBreakfast}, and then it's off to my first ${noun1}. I often get caught ${doInClass} in class. No one likes it when the ${occupation} gives us a surprise ${noun2}. My second class is ${noun3} Theory. Dr. ${randomName} is incredibly ${adjective1}, but I never ${verb} her because of her thick ${noun4}. Her ${bodyPart}, however, are really ${adjective2}. After lunch, I have no more ${nounPlural} and I'm free to ${noun5} ${preposition} my friends. Before I go to bed I ${doBeforeBed} a little bit.`;
+
+console.log(sentence);
